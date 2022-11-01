04c3dd7bde483c246adae2d4674f0cfae1d78693
divizia <divizia.di@gmail.com>
2022-11-01 23:23:32 +0200

    example sending audio files

--- src/main/java/io/project/SpringDemoBot/TelegramBot.java
+++ src/main/java/io/project/SpringDemoBot/TelegramBot.java
@@ -1,59 +1,48 @@
-package io.project.SpringDemoBot.service;
+package io.project.SpringDemoBot;
 
 import com.vdurmont.emoji.EmojiParser;
-import io.project.SpringDemoBot.config.BotConfig;
 import io.project.SpringDemoBot.model.User;
 import io.project.SpringDemoBot.model.UserRepository;
+import io.project.SpringDemoBot.service.TelegramService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
-import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.List;
 
 @Slf4j
 @Component //automatically create an example
 public class TelegramBot extends TelegramLongPollingBot {
-    @Autowired
-    private UserRepository userRepository;
-    final BotConfig config;
+
+    @Value("${bot.token}")
+    private String botToken;
+
+    @Value("${bot.name}")
+    private String botUsername;
+
+    private final UserRepository userRepository;
+    private final TelegramService telegramService;
+
     static final String HELP_TEXT = "This bot is created to demonstrate Spring capabilities\n" +
             "type /mydata to get your data stored";
 
-    public TelegramBot(BotConfig config) {
-        this.config = config;
-        List<BotCommand> listOfCommands = new ArrayList<>();
-        listOfCommands.add(new BotCommand("/start", "get a welcome message"));
-        listOfCommands.add(new BotCommand("/mydata", "get your data stored"));
-        listOfCommands.add(new BotCommand("deletedata", "delete my data"));
-        listOfCommands.add(new BotCommand("help", "info, how to use this bot"));
-        listOfCommands.add(new BotCommand("settings", "set your preferences"));
-
-        try {
-            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
-        } catch (TelegramApiException e) {
-            log.error("Error setting bots command list" + e.getMessage());
-        }
+    public TelegramBot(UserRepository userRepository, TelegramService telegramService) {
+        this.userRepository = userRepository;
+        this.telegramService = telegramService;
+        telegramService.setCommands();
     }
-
 
     @Override
     public String getBotUsername() {
-        return config.getBotName();
+        return botUsername;
     }
 
     @Override
     public String getBotToken() {
-        return config.getToken();
+        return botToken;
     }
 
     @Override
@@ -68,10 +57,13 @@ public class TelegramBot extends TelegramLongPollingBot {
                     startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
                     break;
                 case "/help":
-                    sendMessage(chatId, HELP_TEXT);
+                    telegramService.sendMessage(chatId, HELP_TEXT);
+                    break;
+                case "/audio":
+                    telegramService.sendAudio(chatId, "music/test.mp3");
                     break;
                 default:
-                    sendMessage(chatId, "Sorry, command was not recognized");
+                    telegramService.sendMessage(chatId, "Sorry, command was not recognized");
 
             }
         }
@@ -97,19 +89,8 @@ public class TelegramBot extends TelegramLongPollingBot {
         //String answer = "Hi " + name + " nice to meet you!";
         String answer = EmojiParser.parseToUnicode("Hi " + name + " nice to meet you!" + ":leaves:");
         log.info("Replied to user: " + name);
-        sendMessage(chatId, answer);
+        telegramService.sendMessage(chatId, answer);
 
     }
 
-    private void sendMessage(long chatId, String textToSend) {
-        SendMessage message = new SendMessage();
-        message.setChatId(String.valueOf(chatId));
-        message.setText(textToSend);
-        try {
-            execute(message);
-        } catch (TelegramApiException e) {
-            log.error("Error occurred:" + e.getMessage());
-
-        }
-    }
 }
